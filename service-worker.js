034221ae50be6d1c57b3a430e66131862774c670
wathiq aljazzi <wathiqaljazzi55@gmail.com>
2025-12-27 01:55:42 +0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'tazakar-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;600&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
